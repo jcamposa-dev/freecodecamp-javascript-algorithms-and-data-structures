fd3c004b02d91600c4613ae4775c48122e3777ef
Jossie Campos A. <jcamposa.dev@gmail.com>
2026-09-05 22:09:31 -0600

    Refactor code structure for improved readability and maintainability

---
+++ 02-build-a-sentence-maker/script.js
@@ -0,0 +1,49 @@
+let adjective = "tall";
+let noun = "dog";
+let verb = "run";
+let place = "park";
+let adjective2 = "black";
+let noun2 = "bread";
+
+let firstStory =
+    "Once upon a time, there was a(n) " +
+    adjective +
+    " " +
+    noun +
+    " who loved to eat " +
+    noun2 +
+    ". The " +
+    noun +
+    " lived in a " +
+    place +
+    " and had " +
+    adjective2 +
+    " nostrils that blew fire when it was " +
+    verb +
+    ".";
+console.log("First story: " + firstStory);
+
+adjective = "fat";
+noun = "cat";
+verb = "walk";
+place = "stadium";
+adjective2 = "beige";
+noun2 = "taco";
+
+let secondStory =
+    "Once upon a time, there was a(n) " +
+    adjective +
+    " " +
+    noun +
+    " who loved to eat " +
+    noun2 +
+    ". The " +
+    noun +
+    " lived in a " +
+    place +
+    " and had " +
+    adjective2 +
+    " nostrils that blew fire when it was " +
+    verb +
+    ".";
+console.log("Second story: " + secondStory);
