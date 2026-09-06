7c7159d96b18edb93c92f7ccda31c91d74b840d9
Jossie Campos A. <jcamposa.dev@gmail.com>
2026-09-05 21:48:58 -0600

    add initial HTML structure and JavaScript trivia bot script

---
+++ 01-build-a-javascript-trivia-bot/script.js
@@ -0,0 +1,28 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Jossie";
+let botLocation = "Costa Rica";
+let favoriteLanguage = "javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+    favoriteLanguage +
+    " also is used to create mobile apps and backend solutions.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is fundamental for frontend design.";
+console.log(codingFact);
+
+codingFact =
+    favoriteLanguage + " is a dinamic code language, unlike CSS and HTML.";
+console.log(codingFact);
+
+console.log(
+    "It was fun sharing these facts with you. Goodbye! - " +
+        botName +
+        " from " +
+        botLocation +
+        ".",
+);
